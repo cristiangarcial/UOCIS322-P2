083c5835a8de89947dc5edb9ac72b442892f4024
Cristian Garcia Leon <cgarcial@testium.cs.uoregon.edu>
2021-04-25 22:30:06 -0700

    still working trying to figure out how flask works with input

--- web/app.py
+++ web/app.py
@@ -1,14 +1,28 @@
 """
-John Doe's Flask API.
+Cristian Garcia Leon's Flask API.
 """
-
-from flask import Flask
+import config
+import os
+from flask import Flask, render_template, send_from_directory, abort
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+options = config.configuration()
+DOCROOT = options.DOCROOT
+
+@app.route("/path:page")
+def hello(page):
+    options = get_options()
+    DOCROOT = options.DOCROOT
     return "UOCIS docker demo!\n"
+
+@app.errorhandler(404)
+def not_found(e):
+    return send_from_directory('templates', '404.html'), 404
+
+@app.errorhandler(403)
+def forbidden:
+    return send_from_directory('templates', '403.html'), 403
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
